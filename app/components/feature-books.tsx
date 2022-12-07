d63bc5a933456f81c84f495602b1018699119b00
falvarador <alvaguez1990@gmail.com>
2022-12-06 21:40:40 -0600

    Add responsive styles

--- app/components/feature-books.tsx
+++ app/components/feature-books.tsx
@@ -9,7 +9,7 @@ export default function FeatureBooks() {
         {subtitle}
       </p>
       <div className='mt-12 flex flex-col items-center justify-evenly border-t border-b border-octonary'>
-        <h2 className='mb-14 -mt-8 bg-primary px-16 text-center font-prata text-5xl'>
+        <h2 className='mb-14 -mt-8 bg-primary text-center font-prata text-5xl'>
           Featured Books
         </h2>
         <aside className='flex flex-col gap-5 p-2 md:flex-row lg:gap-10'>
